cc0c94e1dae06cd10f0856f678525978284981e2
Фидан Мамедова <gold_wolverine@mail.ru>
2023-07-21 22:12:22 +0400

    MyConverter

---
+++ ASP.NET projects/MyConv/Check/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "1.0958";
+        double number;
+
+        if (Double.TryParse(input, NumberStyles.Float, CultureInfo.InvariantCulture, out number))
+        {
+            string formattedNumber = number.ToString("R", CultureInfo.InvariantCulture);
+            Console.WriteLine(formattedNumber);
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
